3e7f194bfde4b105cf5073e6993ba182f98b2825
Alex Stezar <alex.stezar07@gmail.com>
2023-11-27 21:00:44 +0200

    Lab8 done

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace Stezar_Alex_Lab7
+﻿using System;
+using Stezar_Alex_Lab7.Data;
+using System.IO;
+
+namespace Stezar_Alex_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
