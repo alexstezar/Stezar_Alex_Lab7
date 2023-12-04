7b70bbacf34aaed7edda33f0f0b30146a0b9f383
Alex Stezar <alex.stezar07@gmail.com>
2023-12-04 23:47:47 +0200

    Lab9 without last ex

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Stezar_Alex_Lab7.Models;
 
+
 namespace Stezar_Alex_Lab7.Data
 {
     public class ShoppingListDatabase
@@ -16,6 +17,30 @@ namespace Stezar_Alex_Lab7.Data
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
+        }
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if(product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
         }
 
         public Task<List<ShopList>> GetShopListsAsync()
@@ -45,6 +70,27 @@ namespace Stezar_Alex_Lab7.Data
         public Task<int> DeleteShopListAsync(ShopList slist)
         {
             return _database.DeleteAsync(slist);
+        }
+
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if(listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+                "select P.ID, P.Description from Product P"
+                + " inner join ListProduct LP"
+                + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+                shoplistid);
         }
     }
 }
