c6cf968c162a1f9a026713ec74746acc04cd25b5
Alex Stezar <alex.stezar07@gmail.com>
2023-12-08 16:58:46 +0200

    ex 10 didn't work

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace Stezar_Alex_Lab7.Models
 {
@@ -15,5 +17,8 @@ namespace Stezar_Alex_Lab7.Models
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
+
+        [SQLiteNetExtensions.Attributes.ForeignKey(typeof(Shop))]       
+        public int ShopID { get; set; }
     }
 }
